51812c4cd8eba784e940783020b0d61f43557bf1
manga_osyo <manga.osyo@gmail.com>
2015-02-18 14:57:07 +0900

    Fix call invoke.

--- lib/iolite/functinal/send.rb
+++ lib/iolite/functinal/send.rb
@@ -1,7 +1,11 @@
+# require "iolite/lambda"
+require "iolite/functinal/invoke"
+
 module Iolite module Functinal
 	def send func, method, *args_, &block
-		func.class.new { |*args|
-			func.call(*args).send(method, *invoke_a(args_, *args), &block)
+		Lambda.new { |*args|
+			invoke(func, *args).send(method, *invoke_a(args_, *args), &block)
+# 			func.call(*args).send(method, *invoke_a(args_, *args), &block)
 		}
 	end
 	module_function :send
