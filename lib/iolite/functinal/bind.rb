df7adcc0fa09ab5002008f8effdada136ab98703
manga_osyo <manga.osyo@gmail.com>
2015-02-20 12:53:28 +0900

    Remove comment out.

--- lib/iolite/functinal/bind.rb
+++ lib/iolite/functinal/bind.rb
@@ -2,7 +2,6 @@ require "iolite/functinal/invoke"
 
 module Iolite module Functinal
 	def bind func, *args_
-# 		Functinal.apply(lambda { |*args| func }, *args_)
 		Iolite.lambda { |*args|
 # 			func.call(*invoke_a(args_, *args))
 			invoke(func, *args).call(*invoke_a(args_, *args))
