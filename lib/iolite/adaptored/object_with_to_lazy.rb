6f1173e4bca0cfed7ba7aa40252bfd1cded4011e
manga_osyo <manga.osyo@gmail.com>
2015-02-26 14:23:29 +0900

    Fix.

--- lib/iolite/adaptored/object_with_to_lazy.rb
+++ lib/iolite/adaptored/object_with_to_lazy.rb
@@ -1,9 +1,5 @@
 require "iolite/adaptor/to_lazy"
 
-module Iolite module Refinements
-	module ObjectWithToLazy
-		refine Object do
-			include Iolite::Adaptor::ToLazy
-		end
-	end
-end end
+class Object
+	include Iolite::Adaptor::ToLazy
+end
