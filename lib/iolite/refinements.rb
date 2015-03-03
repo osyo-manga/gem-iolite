f05c230c6b65c07763565e6432e4804a2c207b8e
manga_osyo <manga.osyo@gmail.com>
2015-03-03 09:15:25 +0900

    Remove refinements.

--- lib/iolite/refinements.rb
+++ lib/iolite/refinements.rb
@@ -1,5 +1,6 @@
 require "iolite/refinements/object_with_to_lazy"
-require "iolite/refinements/array"
-require "iolite/refinements/hash"
-require "iolite/refinements/proc"
+# require "iolite/refinements/array"
+# require "iolite/refinements/hash"
+# require "iolite/refinements/proc"
+# require "iolite/refinements/string"
 
